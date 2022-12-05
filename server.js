20ca9e71d674d700da34e326d23712fcc474b123
Ritik2221999 <bhardwajritik51@gmail.com>
2022-12-05 11:29:43 +0530

    Completed Error handeling

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTIONS! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -24,6 +30,14 @@ mongoose
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listens to port ${PORT}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTIONS! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
