3e92f6b2e4e8433dac457af47daeb6e022107e3a
Ritik2221999 <bhardwajritik51@gmail.com>
2022-12-06 13:32:35 +0530

    Started with the authentication and authorization of users and tours

---
+++ models/userModel.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // this only works on CREATE OR SAVE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+// Encryption of password
+userSchema.pre('save', async function (next) {
+  // Only run this function if passsword was actually modified
+  if (!this.isModified('password')) return next();
+
+  //   hash password with  cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //   Delete PaswordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
